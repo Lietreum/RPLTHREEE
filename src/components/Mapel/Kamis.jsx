131c657a402d1b9655886337e4846b83a986f810
Hoshino <127820998+AkaneAqua@users.noreply.github.com>
2024-07-18 09:02:52 +0700

    Update Kamis.jsx

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -51,9 +51,18 @@ const Kamis = () => {
           10.00-11.45
         </div>
       </div>
-
       <div
-        className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+        className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+        data-aos="fade-up"
+        data-aos-duration="700"
+      >
+        <div className="w-[50%] text-base  font-medium">Ishoma</div>
+        <div className="flex justify-center items-center text-sm">
+          11.45-12.30
+        </div>
+      </div>
+      <div
+        className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
         data-aos="fade-up"
         data-aos-duration="600"
       >
@@ -61,17 +70,15 @@ const Kamis = () => {
         <div className="flex justify-center items-center text-sm">
           12.30-13.15
         </div>
-        <div
-          className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
-          data-aos="fade-up"
-          data-aos-duration="600"
-        >
-          <div className="w-[50%] text-base  font-medium">
-            Pendidikan Pancasila
-          </div>
-          <div className="flex justify-center items-center text-sm">
-            13.15-14.00
-          </div>
+      </div>
+      <div
+        className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+        data-aos="fade-up"
+        data-aos-duration="600"
+      >
+        <div className="w-[50%] text-base  font-medium">PPKN</div>
+        <div className="flex justify-center items-center text-sm">
+          13.15-14.00
         </div>
       </div>
     </>
