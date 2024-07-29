c14533c698a49eba05716c8236156c6fa9ca1059
Hoshino <127820998+AkaneAqua@users.noreply.github.com>
2024-07-29 08:16:57 +0700

    Update Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -42,16 +42,6 @@ const Selasa = () => {
         </div>
       </div>
       <div
-        className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
-        data-aos="fade-up"
-        data-aos-duration="600"
-      >
-        <div className="w-[50%] text-base  font-medium">KK2RPL</div>
-        <div className="flex justify-center items-center text-sm">
-          10.15-11.45
-        </div>
-      </div>
-      <div
         className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
         data-aos="fade-up"
         data-aos-duration="700"
@@ -66,9 +56,19 @@ const Selasa = () => {
         data-aos="fade-up"
         data-aos-duration="600"
       >
-        <div className="w-[50%] text-base  font-medium">KK2RPL</div>
+        <div className="w-[50%] text-base  font-medium">KK4RPL</div>
         <div className="flex justify-center items-center text-sm">
-          12.30-15.30
+          12.30-14.00
+        </div>
+      </div>
+      <div
+        className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+        data-aos="fade-up"
+        data-aos-duration="600"
+      >
+        <div className="w-[50%] text-base  font-medium">Bhs Sunda</div>
+        <div className="flex justify-center items-center text-sm">
+          14.00-15.30
         </div>
       </div>
     </>
